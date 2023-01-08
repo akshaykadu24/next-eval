94be8a664c451037c4488e5da61d0c896f37a9b3
akshay <akshaykadu24@gmail.com>
2023-01-09 03:13:40 +0530

    done

---
+++ components/profile.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+// // import axios from "axios"
+// const Profile = (Prof) => {
+//     console.log(Prof)
+//   return (
+//     <div>Profile</div>
+//   )
+// }
+
+// export async function getServerSideProps(){
+//     let res = await axios.get("https://api.github.com/users/riteshf")  
+//     // let data =  await res.json()
+//     console.log(res.data)
+//     return{
+//         props:{
+//             Prof:res.data
+//         }
+//     }
+// }
+
+// export default Profile
+
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
+import Profile from '../components/profile'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      {/* <Profile/> */}
+      <Component {...pageProps} />
+    </div>
+  )
 }
